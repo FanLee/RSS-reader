0fc6582e8dd7d749e1612f1b5482a6e5f1932451
FanLee <fanlee88@gmail.com>
2016-03-31 10:45:02 +0300

    Initial Commit

---
+++ RSS-reader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RSS-reader
+//
+//  Created by FanLee on 31/03/16.
+//  Copyright © 2016 Vasili Krasnouski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
